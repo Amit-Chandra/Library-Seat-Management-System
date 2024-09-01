959f261b0c055df7042eadbcd75a2d713716b435
Amit Chandra <ask.amitchandra@gmail.com>
2024-09-01 11:41:00 +0530

    Modify

--- users/mixins.py
+++
@@ -1,6 +0,0 @@
-# users/mixins.py
-from django.contrib.auth.mixins import UserPassesTestMixin
-
-class AdminRequiredMixin(UserPassesTestMixin):
-    def test_func(self):
-        return self.request.user.role == 'admin'
